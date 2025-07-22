ac675d30c914ac31ec950d07feed587f108acb03
VanshsharmaMIET <sharma.vansh1312@gmail.com>
2025-07-22 16:35:29 +0530

    Your meaningful commit message

--- src/Components/About/About.jsx
+++ src/Components/About/About.jsx
@@ -29,7 +29,7 @@ const About = () => {
         </div>
       </div>
 
-      <div className="about-achievements">
+      {/* <div className="about-achievements">
         <div className="about-achievement">
         <h1>10+</h1> 
         <p>YEAR OF EXPERIENCE</p>
@@ -47,7 +47,7 @@ const About = () => {
 
 
 
-      </div>
+      </div> */}
     </div>
   )
 }
--- src/assets/services_data.js
+++ src/assets/services_data.js
@@ -7,12 +7,12 @@ const Services_Data = [
     {
         s_no:"02",
         s_name:"Graphics design",
-        s_desc:"Web development is the process of building, programming..."
+        s_desc:"Graphic design is the art of visually communicating ideas through typography, imagery, color, and layout."
     },
     {
         s_no:"03",
         s_name:"Social media",
-        s_desc:"Web development is the process of building, programming..."
+        s_desc:"Social media networking connects people and communities online to share content, ideas, and build relationships."
     },
     // {
     //     s_no:"04",
@@ -27,7 +27,7 @@ const Services_Data = [
     {
         s_no:"04",
         s_name:"Content writing",
-        s_desc:"Web development is the process of building, programming..."
+        s_desc:"Content writing is the craft of creating engaging, informative, and purposeful text for digital or print platforms."
     },
 ]
  
